aed4497f8fe400a401f614d47217c89e67b7aebe
90felipe09 <felhipeshiraishi@gmail.com>
2021-06-20 15:36:48 -0300

    add: hyperledger connection class

---
+++ src/hyperledger/config.ts
@@ -0,0 +1,5 @@
+// A ideia aqui é guardar os nomes padrão de como estariam lá na hyperledger que o toshi subiu.
+// Se o nome do canal mudar ou do smart contract, só mudar aqui.
+
+export const PAYMENT_CHANNEL="PaymentChannel"
+export const MICROPAYMENT_CHAINCODE="MicropaymentChaincode"
---
+++ src/hyperledger/connections/GatewayConnection.ts
@@ -0,0 +1,65 @@
+import { Contract, Gateway, GatewayOptions, Network, X509Identity } from 'fabric-network';
+import { IAuthenticatedMessageData } from '../../torrente/messages/models/AuthenticatedMessage';
+import { MICROPAYMENT_CHAINCODE, PAYMENT_CHANNEL } from '../config';
+
+export class GatewayConnection {
+    private clientIdentity: X509Identity;
+
+    public peerGateway: Gateway;
+    public gatewayNetwork: Network;
+    public chaincode: Contract;
+
+    public constructor (credentials: IAuthenticatedMessageData){
+        this.clientIdentity = this.getIdentity(credentials);
+    }
+
+    private connectToPeerGateway = async(): Promise<Gateway> => {
+        if (!this.peerGateway) {
+            const gatewayOptions: GatewayOptions = {
+                identity: this.clientIdentity,
+            };
+            const gateway = new Gateway();
+            await gateway.connect({}, gatewayOptions);
+            this.peerGateway = gateway;
+        }
+        return this.peerGateway;
+    }
+
+    private getChannelNetwork = async(channelName: string): Promise<Network> => {
+        if (!this.gatewayNetwork){
+            const network = await this.peerGateway.getNetwork(channelName);
+            this.gatewayNetwork = network;
+        }
+        
+        return this.gatewayNetwork;
+    }
+
+    private getChaincodeReference = async(chaincodeId: string): Promise<Contract> => {
+        if (!this.chaincode){
+            const contract = this.gatewayNetwork.getContract(chaincodeId);
+            this.chaincode = contract;
+        }
+        
+        return this.chaincode;
+    }
+
+    public getMicropaymentChaincodeReference = async (): Promise<Contract> => {
+        await this.connectToPeerGateway();
+        await this.getChannelNetwork(PAYMENT_CHANNEL);
+        const chaincode = await this.getChaincodeReference(MICROPAYMENT_CHAINCODE);
+        return chaincode;
+    }
+
+    private getIdentity = (credentials: IAuthenticatedMessageData): X509Identity => {
+        const x509identity: X509Identity = {
+            credentials: {
+                certificate: credentials.certificate,
+                privateKey: credentials.privateKey,
+            },
+            mspId: credentials.orgMSP,
+            type: "X.509"
+        }
+    
+        return x509identity
+    }
+}
