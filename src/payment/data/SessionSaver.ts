cd61c169ea7844457b56794731f644d7d2ee04a0
90felipe09 <Felhipeshiraishi@gmail.com>
2021-06-21 19:04:36 -0300

    add: saver and loader of session data

---
+++ src/payment/data/SessionSaver.ts
@@ -0,0 +1,67 @@
+import { AES } from "crypto-js";
+import sha256 from 'crypto-js/sha256';
+import fs from 'fs';
+import { PaymentHandler } from "../controllers/PaymentHandler";
+import { ReceiverHandler } from "../controllers/ReceiverHandler";
+import { SessionController } from "../controllers/SessionController";
+import { PayerHandlerData, ReceiverHandlerData, SessionData } from "./SessionData";
+
+export class SessionSaver{
+    private static convertSessionObject = (session: SessionController): SessionData =>{
+        const paymentHandlers = [];
+        const receiverHandlers = [];
+
+        Object.values(session.paymentHandlers).map((paymentHandler: PaymentHandler) => {
+            const [ lastHashSent, lastHashSentIndex ] = paymentHandler.retryLastPayment();
+            const newPaymentHandler: PayerHandlerData = {
+                commitment: paymentHandler.commitment.commitmentMessage,
+                hashChain: paymentHandler.hashChain.hashes,
+                ip: paymentHandler.receiverIp,
+                lastHashSent: lastHashSent,
+                lastHashSentIndex: lastHashSentIndex,
+                magneticLink: paymentHandler.commitment.commitmentMessage.content.magneticLink
+            }
+
+            paymentHandlers.push(newPaymentHandler);
+        })
+
+        Object.values(session.receivingListeners).map((receiverHandler: ReceiverHandler) => {
+            const newPaymentHandler: ReceiverHandlerData = {
+                commitment: receiverHandler.commitment,
+                ip: receiverHandler.payerIp,
+                lastHashReceived: receiverHandler.lastHash,
+                lastHashReceivedIndex: receiverHandler.lastHashIndex,
+                lastHashRedeemed: receiverHandler.lastHashRedeemed,
+                lastHashRedeemedIndex: receiverHandler.lastHashRedeemedIndex,
+                publicCertificate: receiverHandler.loadedPayerPublicKey
+            }
+
+            receiverHandlers.push(newPaymentHandler);
+        })
+
+        return {
+            payerHandlers: paymentHandlers,
+            receiverHandlers: receiverHandlers
+        }
+    }
+
+    public static saveSession(session: SessionController){
+        const certificate = session.loadedUserCertificate;
+        const sessionToSave = `${sha256(certificate)}.pay`;
+
+        const privateKey = session.loadedUserKey;
+        const keyToEncrypt = sha256(privateKey).toString();
+
+        const sessionData = SessionSaver.convertSessionObject(session);
+
+        const contentToCypher = JSON.stringify(sessionData);
+
+        const cypheredData = AES.encrypt(contentToCypher, keyToEncrypt).toString();
+
+        fs.writeFile(sessionToSave, cypheredData, 'utf8', function (err) {
+            if (err) {
+              return console.log(err);
+            }
+          });
+    }
+}
