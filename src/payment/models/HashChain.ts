171692aec2349e5ea58889cce8d20918d5f98af0
90felipe09 <Felhipeshiraishi@gmail.com>
2021-06-21 19:03:13 -0300

    refactor: commit and hashchain have init and load

--- src/payment/models/HashChain.ts
+++ src/payment/models/HashChain.ts
@@ -4,13 +4,24 @@ export class HashChain {
     hashes: string[];
     hashToPay: number;
 
-    public constructor (size: number){
+    public initHashChain = (size: number) => {
         const firstHash: string = sha256(Math.random().toString()).toString();
         this.hashes = [firstHash];
         for (let index = 0; index < size; index++) {
             this.hashes.push(sha256(this.hashes[index]).toString());
         }
         this.hashToPay = size - 1;
+    }
+
+    public setHashChain = (hashes: string[], lastHash: string, lastHashIndex: number) => {
+        this.hashes = hashes;
+        if (this.hashes[lastHashIndex] === lastHash){
+            this.hashToPay = lastHashIndex - 1;
+        }
+    }
+
+    public payHashIndex(): number {
+        return this.hashes.length - this.hashToPay - 1;
     }
 
     public payHash(): string {
