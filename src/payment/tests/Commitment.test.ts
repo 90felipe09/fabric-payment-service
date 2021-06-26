887a59d01e4b7ad2de3765b3f5d57469a86c43ae
90felipe09 <Felhipeshiraishi@gmail.com>
2021-06-25 22:37:10 -0300

    add: executable package build

--- src/payment/tests/Commitment.test.ts
+++ src/payment/tests/Commitment.test.ts
@@ -4,32 +4,35 @@ import { HashChain } from "../models/HashChain";
 describe("test Commitment class", () => {
     const torrentIdTest = "torrentIdTest";
     const receiverPublicKeyTest = "receiverPublicKeyTest";
-    const hashChainTest = new HashChain(2);
+    const hashChainTest = new HashChain();
+    hashChainTest.initHashChain(2);
     const privateKeyStringTest = "-----BEGIN PRIVATE KEY-----\r\nMIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgAuzqAU+NF2D1TqOs\r\nIFAiowyTvHcja6JaBsTpzXJI56uhRANCAAQmoCGwk9+Af9T6YdPez3Dboh6SUcze\r\nnoXqg5JeNr2sh0vuG/Y9Qw/phidXg6N665mM+mhk3jilIyz3ciPa6CX1\r\n-----END PRIVATE KEY-----\r\n";
 
-    const testCommitment = new Commitment(
+    const testCommitment = new Commitment();
+    testCommitment.initCommitment(
         torrentIdTest,
         receiverPublicKeyTest,
         hashChainTest.getHashRoot(),
-        privateKeyStringTest
-    );
+        privateKeyStringTest,
+        receiverPublicKeyTest
+    )
 
     it("should have signature validated.", async () => {
         var falseCommitment: CommitmentMessage = {
             content: {
-                torrentId: torrentIdTest,
-                receiverPublicKey: receiverPublicKeyTest,
-                hashRoot: "fake hash",
-                payerPublicKey: testCommitment.commitmentMessage.content.payerPublicKey,
+                magneticLink: torrentIdTest,
+                payerFingerprint: receiverPublicKeyTest,
+                receiverFingerprint: receiverPublicKeyTest,
+                hashRoot: "fake hash"
             },
             signature: testCommitment.commitmentMessage.signature
         }
 
-        const truthyValidation = Commitment.validateSignature(testCommitment.commitmentMessage);
+        const truthyValidation = Commitment.validateSignature(testCommitment.commitmentMessage, receiverPublicKeyTest);
         expect(truthyValidation).toBeTruthy();
         console.log(testCommitment.commitmentMessage.content);
         console.log("\n\t" + truthyValidation);
-        const fakeValidation = Commitment.validateSignature(falseCommitment);
+        const fakeValidation = Commitment.validateSignature(falseCommitment, receiverPublicKeyTest);
         expect(fakeValidation).toBeFalsy();
         console.log(falseCommitment.content);
         console.log("\n\t" + fakeValidation);
