5cdec2510086559757755e7d4af30b93d41010a3
90felipe09 <Felhipeshiraishi@gmail.com>
2021-07-10 17:07:42 -0300

    add: download intention message model

--- src/torrente/messages/models/MessageModel.ts
+++ src/torrente/messages/models/MessageModel.ts
@@ -2,7 +2,8 @@ export enum MessagesTypesEnum {
     DownloadedBlock = "DownloadedBlock",
     Authenticated = "Authenticated",
     Closing = "Closing",
-    Logout = "Logout"
+    Logout = "Logout",
+    DownloadIntention = "DownloadIntention"
 }
 
 export interface IMessagesModel<T> {
