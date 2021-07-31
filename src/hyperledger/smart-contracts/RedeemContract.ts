f2ad4c10bb8df664eca34fe84d078a4deb1fe865
90felipe09 <Felhipeshiraishi@gmail.com>
2021-07-31 17:30:40 -0300

    add: redeem use case to contract ref

--- src/hyperledger/smart-contracts/RedeemContract.ts
+++ src/hyperledger/smart-contracts/RedeemContract.ts
@@ -1,8 +1,8 @@
 import { Contract } from "fabric-network";
-import { CommitmentContent, CommitmentMessage } from "../../payment/models/Commitment";
+import { CommitmentMessage } from "../../payment/models/Commitment";
 import { IAuthenticatedMessageData } from "../../torrente/messages/models/AuthenticatedMessage";
 import { REDEEM_CONTRACT } from "../config";
-import { SmartContract } from "./SmartContract"; 
+import { SmartContract } from "./SmartContract";
 
 export class RedeemContract extends SmartContract {
     chaincodeReference: Contract;
@@ -11,7 +11,8 @@ export class RedeemContract extends SmartContract {
         super(credentials, REDEEM_CONTRACT);
     }
 
-    public invokeRedeem = (commitment: CommitmentMessage, hashLink: string, hashLinkIndex: number) => {
-        this.invokeTransaction('redeem', [commitment, hashLink, hashLinkIndex]);
+    public invokeRedeem = async(commitment: CommitmentMessage, hashLink: string, hashLinkIndex: number): Promise<void> => {
+        const stringfiedCommitment: string = JSON.stringify(commitment);
+        this.invokeTransaction('redeem', [stringfiedCommitment, hashLink, hashLinkIndex]);
     }
 }
