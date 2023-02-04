359d7ead8b7985843a066b73f3bd94bcab9e2437
90felipe09 <felhipeshiraishi@gmail.com>
2023-02-03 22:53:26 -0300

    refactor: change hyperledger endpoint and contract

--- src/hyperledger/config.ts
+++ src/hyperledger/config.ts
@@ -1,14 +1,14 @@
 // A ideia aqui é guardar os nomes padrão de como estariam lá na hyperledger que o toshi subiu.
 // Se o nome do canal mudar ou do smart contract, só mudar aqui.
 
-export const PAYMENT_CHANNEL="channel2"
+export const PAYMENT_CHANNEL="mychannel"
 export const CHAINCODE_ID="currency"
 export const PAYMENT_INTENTION_CONTRACT="PaymentIntentionContract"
 export const ACCOUNT_CONTRACT="AccountContract"
 export const REDEEM_CONTRACT="RedeemContract"
 // export const HYPERLEDGER_IP = "143.107.111.58"
-export const HYPERLEDGER_DNS = "hyperledger.larc.usp.br"
-export const HYPERLEDGER_IP = "http://hyperledger.larc.usp.br"
+export const HYPERLEDGER_DNS = "amazonbiobank.duckdns.org"
+export const HYPERLEDGER_IP = "http://amazonbiobank.duckdns.org"
 export const HYPERLEDGER_PORT = "7051"
 export const HYPERLEDGER_CONNECTION_PROFILE_PORT = "3003"
 export const HYPERLEDGER_CONNECTION_PROFILE_ENDPOINT = "connection-profile"
